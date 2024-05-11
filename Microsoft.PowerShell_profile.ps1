ed83ef14bbf191eceb7f4732d85edf474f9a1454
BetaBrainz <harrshnair@outlook.com>
2024-05-11 13:24:40 +0530

    Update to v1.5

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 ### PowerShell Profile Refactor
-### Version 1.03 - Refactored
+### Version 1.05 - Refactored
 
 # Initial GitHub.com connectivity check with 1 second timeout
 $canConnectToGitHub = Test-Connection github.com -Count 1 -Quiet -TimeoutSeconds 1
@@ -16,28 +16,46 @@ if (Test-Path($ChocolateyProfile)) {
 }
 
 # Check for Profile Updates
-#function Update-Profile {
-#    if (-not $global:canConnectToGitHub) {
-#        Write-Host "Skipping profile update check due to GitHub.com not responding within 1 second." -ForegroundColor Yellow
-#        return
-#    }
+function UpdateProfile {
+    param (
+        [switch]$ForceUpdate = $false
+    )
 
-#    try {
-#        $url = "https://raw.githubusercontent.com/ChrisTitusTech/powershell-profile/main/Microsoft.PowerShell_profile.ps1"
-#        $oldhash = Get-FileHash $PROFILE
-#        Invoke-RestMethod $url -OutFile "$env:temp/Microsoft.PowerShell_profile.ps1"
-#        $newhash = Get-FileHash "$env:temp/Microsoft.PowerShell_profile.ps1"
-#        if ($newhash.Hash -ne $oldhash.Hash) {
-#            Copy-Item -Path "$env:temp/Microsoft.PowerShell_profile.ps1" -Destination $PROFILE -Force
-#            Write-Host "Profile has been updated. Please restart your shell to reflect changes" -ForegroundColor Magenta
-#       }
-#    } catch {
-#        Write-Error "Unable to check for `$profile updates"
-#    } finally {
-#        Remove-Item "$env:temp/Microsoft.PowerShell_profile.ps1" -ErrorAction SilentlyContinue
-#    }
-#}
-#Update-Profile
+    $lastUpdated = (Get-Item $PROFILE).LastWriteTime
+    $timeSinceUpdate = (Get-Date) - $lastUpdated
+    $nextUpdateDue = $lastUpdated.AddDays(1)
+
+    if ($timeSinceUpdate.TotalDays -lt 1 -and -not $ForceUpdate) {
+        $nextUpdateInHours = [Math]::Floor((24 - $timeSinceUpdate.TotalHours))
+        $nextUpdateInMinutes = [Math]::Floor((60 - $timeSinceUpdate.TotalMinutes) % 60)
+        Write-Host "Next profile update: $nextUpdateInHours hour(s) and $nextUpdateInMinutes minute(s). Use 'ReloadProfile' to update now." -ForegroundColor Magenta
+        return
+    }
+
+    Write-Host "Initiating profile update check from GitHub......" -ForegroundColor Cyan
+    $tempFile = "$env:temp/Microsoft.PowerShell_profile.ps1"
+
+    try {
+        $url = "https://raw.githubusercontent.com/BetaBrainz/powershell-profile/main/Microsoft.PowerShell_profile.ps1"
+        Invoke-RestMethod $url -OutFile $tempFile
+
+        $oldhash = Get-FileHash $PROFILE -ErrorAction SilentlyContinue
+        $newhash = Get-FileHash $tempFile
+
+        if ($oldhash.Hash -ne $newhash.Hash) {
+            Write-Host "A new version of the profile has been detected. Updating profile..." -ForegroundColor Yellow
+            Copy-Item -Path $tempFile -Destination $PROFILE -Force
+            Write-Host "Profile has been updated successfully. Please restart your shell to reflect changes." -ForegroundColor Magenta
+        } else {
+            Write-Host "Your PowerShell profile is already up to date." -ForegroundColor Green
+        }
+    } catch {
+        Write-Error "Failed to check or update profile. Error: $_"
+    } finally {
+        Remove-Item $tempFile -ErrorAction SilentlyContinue
+    }
+}
+UpdateProfile
 
 function Update-PowerShell {
     if (-not $global:canConnectToGitHub) {
@@ -180,6 +198,8 @@ function pkill($name) {
     Get-Process $name -ErrorAction SilentlyContinue | Stop-Process
 }
 
+function fkill { Stop-Process -Name $args[0] }
+
 function pipupdate($name) {
     pip install --upgrade ((pip freeze) -replace '==.+','')
 }
@@ -246,6 +266,8 @@ function lazyg {
     git push
 }
 
+function ss {git status --short}
+
 # Quick Access to System Information
 function sysinfo { Get-ComputerInfo }
 
@@ -256,6 +278,145 @@ function flushdns { Clear-DnsClientCache }
 function cpy { Set-Clipboard $args[0] }
 
 function pst { Get-Clipboard }
+
+function SystemResourceUsage {
+    $cpuLoad = Get-Counter '\Processor(_Total)\% Processor Time' | Select-Object -ExpandProperty countersamples | Select-Object -ExpandProperty cookedvalue
+    $memUsage = Get-WmiObject Win32_OperatingSystem | Select-Object @{Name="MemoryUsage";Expression={"{0:N2}" -f ((($_.TotalVisibleMemorySize - $_.FreePhysicalMemory)*100)/ $_.TotalVisibleMemorySize)}}
+    Write-Output "CPU Load: $cpuLoad%"
+    Write-Output "Memory Usage: $($memUsage.MemoryUsage)%"
+}
+
+function NetworkSpeed {
+    try {
+        $output = speedtest-cli --simple
+        $lines = $output -split "\n"
+
+        foreach ($line in $lines) {
+            if ($line -like "*Ping*") {
+                Write-Host $line -ForegroundColor Cyan
+            }
+            elseif ($line -like "*Download*") {
+                Write-Host $line -ForegroundColor Green
+            }
+            elseif ($line -like "*Upload*") {
+                Write-Host $line -ForegroundColor Magenta
+            }
+            else {
+                Write-Host $line
+            }
+        }
+    } catch {
+        Write-Host "Failed to perform speed test. Error: $_" -ForegroundColor Red
+    }
+}
+
+function wifinetwork{netsh wlan show profile}
+
+function wifinetworkfun{netsh wlan show profile $args key=clear | findstr “Key Content”}
+set-alias thiswifi wifinetworkfun
+
+function weatherfun{curl wttr.in/$args}
+set-alias weather weatherfun
+
+function qrfun{curl qrenco.de/$args}
+set-alias qr qrfun
+
+function googleSearch{start www.google.com/search?q=$args}
+set-alias gs googleSearch
+
+function youtubeSearch{start www.youtube.com/search?q=$args}
+set-alias ys youtubeSearch
+
+function scanfile { sfc /scannow }
+
+function checkhealth { DISM /Online /Cleanup-Image /CheckHealth }
+
+function scanhealth { DISM /Online /Cleanup-Image /ScanHealth }
+
+function restorehealth { DISM /Online /Cleanup-Image /RestoreHealth }
+
+function ipflush { ipconfig /flushdns }
+
+function shutdown { shutdown /s }
+
+function restart { shutdown /r }
+
+function Edit-Profile { notepad++ "$Env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" }
+
+
+function cleaner{	
+	# Function to clean temporary files
+	function Clean-TempFiles {
+		Write-Host "Cleaning temporary files..."
+		Get-ChildItem -Path $env:TEMP -Recurse | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
+	}
+
+	# Function to clean the Recycle Bin
+	function Clean-RecycleBin {
+		Write-Host "Emptying the Recycle Bin..."
+		(New-Object -ComObject Shell.Application).NameSpace(0xA).Items() | ForEach { Remove-Item $_.Path -Force -Recurse -ErrorAction SilentlyContinue }
+	}
+
+	# Function to perform disk cleanup
+	function Perform-DiskCleanup {
+		Write-Host "Performing disk cleanup..."
+		$cleanmgr = Start-Process cleanmgr -ArgumentList "/sagerun:1" -PassThru
+		$cleanmgr.WaitForExit()
+	}	
+
+	# Running all clean-up tasks
+	Clean-TempFiles
+	Clean-RecycleBin
+	Perform-DiskCleanup
+
+	Write-Host "System cleanup complete."
+}
+
+function ipchange{
+# Display network adapters to the user
+Write-Host "Available Network Adapters and their Interface Indexes:"
+Get-NetAdapter | Format-Table Name, InterfaceIndex, Status
+
+# Prompt for Interface Index and validate
+$interfaceIndex = $null
+do {
+    $interfaceIndex = Read-Host "Enter the Interface Index from the list above"
+    if (-not $interfaceIndex -or $interfaceIndex -eq '') {
+        Write-Host "Interface Index cannot be empty, please enter a valid number."
+    }
+    if (-not (Get-NetAdapter | Where-Object InterfaceIndex -eq $interfaceIndex)) {
+        Write-Host "Invalid Interface Index entered. Please enter a number from the list above."
+        $interfaceIndex = $null
+    }
+} while (-not $interfaceIndex)
+
+$ipAddress = Read-Host "Enter the new IP Address"
+$subnetMask = Read-Host "Enter the Subnet Mask"
+
+# Check if subnet mask is empty and set default value
+if ([string]::IsNullOrWhiteSpace($subnetMask)) {
+    $subnetMask = "255.255.255.0"
+}
+
+# Convert the subnet mask to prefix length
+$prefixLength = ConvertTo-PrefixLength -SubnetMask $subnetMask
+
+# Remove the existing IP address
+try {
+    Get-NetIPAddress -InterfaceIndex $interfaceIndex | Remove-NetIPAddress -Confirm:$false
+    Write-Host "Existing IP Address(es) removed."
+} catch {
+    Write-Error "Failed to remove existing IP Address. Error: $_"
+}
+
+# Set the IP Address
+try {
+    New-NetIPAddress -InterfaceIndex $interfaceIndex -IPAddress $ipAddress -PrefixLength $prefixLength
+    Write-Host "IP Address has been changed successfully."
+} catch {
+    Write-Error "Failed to change IP Address. Error: $_"
+}
+}
 
 # Enhanced PowerShell Experience
 Set-PSReadLineOption -Colors @{
@@ -277,4 +438,96 @@ if (Get-Command zoxide -ErrorAction SilentlyContinue) {
     } catch {
         Write-Error "Failed to install zoxide. Error: $_"
     }
+}
+
+function ShowFunctions {
+    Write-Host "Available custom functions with descriptions:" -ForegroundColor Cyan
+
+    # Profile Management
+    Write-Host "`nProfile Management:" -ForegroundColor Green
+    Write-Output "  - UpdateProfile: Checks and updates the PowerShell profile from GitHub. Usage: UpdateProfile -ForceUpdate"
+    Write-Output "  - EditProfile: Opens the current user's all hosts profile in the default editor. Usage: EditProfile"
+    Write-Output "  - ReloadProfile: Reloads the PowerShell profile. Usage: ReloadProfile"
+    Write-Output "  - UpdatePowerShell: Checks and updates PowerShell if a newer version is available. Usage: UpdatePowerShell"
+
+    # System Utilities
+    Write-Host "`nSystem Utilities:" -ForegroundColor Green
+    Write-Output "  - Uptime: Shows system uptime since the last boot. Usage: Uptime"
+    Write-Output "  - Sysinfo: Retrieves detailed system information. Usage: Sysinfo"
+
+    # Network Utilities
+    Write-Host "`nNetwork Utilities:" -ForegroundColor Green
+    Write-Output "  - GetPublicIP: Retrieves the public IP address. Usage: GetPublicIP"
+    Write-Output "  - FlushDNS: Clears the DNS client cache. Usage: FlushDNS"
+    Write-Output "  - IPchange: Tries to change IP allocated to system. Usage: ipchange"
+	Write-Output "  - NetworkSpeed: Test Network Speed. Usage: NetworkSpeed"
+	Write-Output "  - wifinetwork: Get Wifi profiles. Usage: wifinetwork"
+	Write-Output "  - wifinetworkfun: Get Wifi Content. Usage: wifinetworkfun"
+
+    # File Management
+    Write-Host "`nFile Management:" -ForegroundColor Green
+    Write-Output "  - OpenDir: Opens the current directory in Windows Explorer. Usage: OpenDir"
+    Write-Output "  - unzip: Extracts a zip file to the specified directory. Usage: unzip 'file.zip'"
+    Write-Output "  - grep: Searches for patterns in files. Usage: grep 'regex' 'path'"
+    Write-Output "  - df: Displays disk space usage. Usage: df"
+    Write-Output "  - sed: Replaces text in a file. Usage: sed 'file' 'find' 'replace'"
+    Write-Output "  - which: Finds the location of a command. Usage: which 'cmd'"
+    Write-Output "  - head: Displays the first 'n' lines of a file. Usage: head 'file' 10"
+    Write-Output "  - tail: Displays the last 'n' lines of a file. Usage: tail 'file' 10"
+    Write-Output "  - nf: Creates a new file. Usage: nf 'filename'"
+    Write-Output "  - mkcd: Creates a new directory and changes to it. Usage: mkcd 'dirname'"
+    Write-Output "  - touch: Creates or updates the timestamp of a file. Usage: touch 'file'"
+    Write-Output "  - ff: Finds files containing a string in their names. Usage: ff 'pattern'"
+
+    # Process Management
+    Write-Host "`nProcess Management:" -ForegroundColor Green
+    Write-Output "  - pkill: Terminates processes by name. Usage: pkill 'processName'"
+    Write-Output "  - pgrep: Lists all processes by name. Usage: pgrep 'processName'"
+    Write-Output "  - fkill: Force stops a process by name. Usage: fkill 'processName'"
+
+    # Git Utilities
+    Write-Host "`nGit Utilities:" -ForegroundColor Green
+    Write-Output "  - gs: Runs the 'git status' command. Usage: gs"
+    Write-Output "  - ga: Stages all changes in git. Usage: ga"
+    Write-Output "  - gc: Commits staged changes in git with a provided message. Usage: gc 'message'"
+    Write-Output "  - gp: Pushes committed changes to a remote git repository. Usage: gp"
+    Write-Output "  - g: Navigates to the GitHub directory. Usage: g"
+    Write-Output "  - gcom: Stages and commits all changes in git with a specified message. Usage: gcom 'message'"
+    Write-Output "  - lazyg: Stages, commits, and pushes all changes in git. Usage: lazyg 'message'"
+
+    # Clipboard Utilities
+    Write-Host "`nClipboard Utilities:" -ForegroundColor Green
+    Write-Output "  - cpy: Copies text to the clipboard. Usage: cpy 'text'"
+    Write-Output "  - pst: Retrieves the current content of the clipboard. Usage: pst"
+    Write-Output "  - CopyCsvToClipboard: Copies contents of a CSV file to the clipboard. Usage: CopyCsvToClipboard 'file.csv'"
+
+    # Navigation Shortcuts
+    Write-Host "`nNavigation Shortcuts:" -ForegroundColor Green
+    Write-Output "  - docs: Navigates to the Documents folder. Usage: docs"
+    Write-Output "  - dtop: Navigates to the Desktop folder. Usage: dtop"
+    Write-Output "  - ep: Opens the current PowerShell profile in the editor. Usage: ep"
+
+    # Listing and Formatting
+    Write-Host "`nListing and Formatting:" -ForegroundColor Green
+    Write-Output "  - la: Lists all items in the current directory, formatted as a table. Usage: la"
+    Write-Output "  - ll: Lists all items, providing detailed information. Usage: ll"
+	
+	# Quality of Life Utilities
+    Write-Host "`nQuality of Life Utilities:" -ForegroundColor Green
+	Write-Output "  - weatherfun: Get Weather information. Usage: weatherfun Location"
+	Write-Output "  - qrfun: Create QR codes. Usage: qrfun url"
+	Write-Output "  - googleSearch: Search google for queries. Usage: googleSearch query"
+	Write-Output "  - youtubeSearch: Search Youtube for queries. Usage: youtubeSearch query"
+	Write-Output "  - scanfile: Runs SFC scan. Usage: scanfile"
+	Write-Output "  - checkhealth: Runs DISM health check. Usage: checkhealth"
+	Write-Output "  - scanhealth: Runs DISM health scan. Usage: scanhealth"
+	Write-Output "  - restorehealth: Runs DISM health restore. Usage: restorehealth"
+	Write-Output "  - ipflush: flushes IP. Usage: ipflush"
+	Write-Output "  - shutdown: Shutdown the system. Usage: shutdown"
+	Write-Output "  - restart: Restarts the system. Usage: restart"
+	Write-Output "  - Get-HWVersion: Get Hardware infomation. Usage: Get-HWVersion"
+	Write-Output "  - Edit-Profile: Edits the profile. Usage: Edit-Profile"
+	Write-Output "  - cleaner: Cleans temp files. Usage: cleaner"
+	Write-Output "  - ipchange: Tries to change IP. Usage: ipchange"
+
 }
